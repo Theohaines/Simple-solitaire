e7a4b716347cf8f62e14949ed451842a60af4c5e
Theohaines <91698052+Theohaines@users.noreply.github.com>
2023-12-07 11:00:27 +0000

    Update main.js

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -2,9 +2,13 @@
 const commandEntry = document.getElementById("commandEntry");
 const terminal = document.getElementById("terminal");
 
+//Terminal vars
+var terminalTextColor = "##1bc615";
+
 //Game vars
 var score = 0;
 var gameInProgress = false;
+var moves = 0;
 
 //Deck and stock
 var deck = [];
@@ -40,7 +44,10 @@ class Card{
 //Terminal stuff (Printing and clearing)
 function printToTerminal(textToPrint){
 	var linebreak = document.createElement("br");
-	var textNode = document.createTextNode(textToPrint); terminal.prepend(linebreak ,textNode);
+	var text = document.createElement("p");
+	text.textContent = textToPrint;
+	text.style.color = terminalTextColor;
+	terminal.prepend(linebreak, textToPrint);
 }
 
 function clearTerminal(){
@@ -98,6 +105,8 @@ function processCommand(){
 		lsCmd(commandEntry.value.toLowerCase());
 	} else if (commandEntry.value.toLowerCase().includes("move")) {
 		moveCard(commandEntry.value.toLowerCase());
+	} else if (commandEntry.value.toLowerCase().includes("color")) {
+		colorCmd(commandEntry.value.toLowerCase());
 	} else {
 		printToTerminal("No command found. Try 'help'")
 	}
@@ -140,6 +149,21 @@ function debugCmd(command){
 		}
 	} else {
 		printToTerminal("Incorrect usage of debug. Try 'help'")
+	}
+	commandEntry.value = '';
+}
+
+function colorCmd(command){
+	command = command.replace("color", "").trim(); // Remove ls part of cmd and all whitespace
+
+	if(command == "0"){
+		debugStock();
+	} else if (command == "1"){
+		debugTableau();
+	} else if (command == "2"){
+		debugBuildPile();
+	} else {
+		printToTerminal("Incorrect usage of color. Try 'help'")
 	}
 	commandEntry.value = '';
 }
